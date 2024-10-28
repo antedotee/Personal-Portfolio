4c7483e449787690ea87ed8e2b5e76a46f5fedb9
antedotee <soniyadav2051982@gmail.com>
2024-10-29 02:13:41 +0530

    fixed minor errors

--- src/components/ui/icon-cloud.tsx
+++ src/components/ui/icon-cloud.tsx
@@ -75,15 +75,9 @@ export default function IconCloud({ iconSlugs }: DynamicCloudProps) {
     if (!data) return null;
 
     return Object.values(data.simpleIcons).map((icon) =>
-      renderCustomIcon(icon, theme || "light"),
+      renderCustomIcon(icon, theme || "light")
     );
   }, [data, theme]);
 
-  return (
-    // @ts-ignore
-    ts-expect-error
-    <Cloud {...cloudProps}>
-      <>{renderedIcons}</>
-    </Cloud>
-  );
+  return <Cloud {...cloudProps}>{renderedIcons}</Cloud>;
 }
